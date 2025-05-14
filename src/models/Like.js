7f2a23deb96b56de9859de1909d7d00ccab11505
João Bernardo <a045900@umaia.pt>
2025-05-14 05:47:19 +0100

    Update Like Model

---
+++ src/models/Like.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const Like = sequelize.define(
+        "Like",
+        {},
+        {
+            tableName: "likes",
+            freezeTableName: true,
+        }
+    );
+
+    Like.associate = (models) => {
+        models.User.belongsToMany(models.Movie, {
+            through: Like,
+            foreignKey: "userId",
+            otherKey: "movieId",
+            onDelete: "CASCADE"
+        });
+        models.Movie.belongsToMany(models.User, {
+            through: Like,
+            foreignKey: "movieId",
+            otherKey: "userId",
+            onDelete: "CASCADE"
+        });
+    };
+
+    return Like;
+};
