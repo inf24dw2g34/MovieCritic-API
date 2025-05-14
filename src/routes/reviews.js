e59ce77ab5b0589824c808ebe2c680e48a208bd0
João Bernardo <a045900@umaia.pt>
2025-05-14 05:48:02 +0100

    Minor adjustment

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -15,7 +15,6 @@ function ensureAdmin(req, res, next) {
 function ensureAuth(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.status(401).json({
-    status: 'error',
     message: 'Not Authenticated',
   });
 }
