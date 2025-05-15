fe99351107e421c03e5cb78d1cba0a73243ad63b
João Bernardo <a045900@umaia.pt>
2025-05-15 21:44:59 +0100

    Added User routes

---
+++ src/routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const user = require("../controllers/user.controller");
+const { ensureAuth, ensureAdmin } = require("../middlewares/auth.middleware");
+
+// Get all users
+router.get("/", ensureAuth, ensureAdmin, user.getUsers);
+
+// Get :id user
+router.get("/:id", ensureAuth, user.getUser);
+
+// Edit :id user
+router.patch("/:id", ensureAuth, user.updateUser);
+
+// Delete :id user [ADMIN ONLY]
+router.delete("/:id", ensureAuth, ensureAdmin, user.deleteUser);
+
+// Get :id user Reviews
+router.get("/:id/reviews", ensureAuth, user.getUserReviews);
+
+// Get :id user likes
+router.get("/:id/likes", ensureAuth, user.getUserLikes);
+
+module.exports = router;
