f3818b31ab15a6a744d790af062613ffd58c612b
João Bernardo <a045900@umaia.pt>
2025-05-17 20:44:09 +0100

    Added production variables

--- src/config/db.js
+++ src/config/db.js
@@ -13,5 +13,19 @@ module.exports = {
             acquire: 30000,
             idle: 10000
         }
+    },
+    production: {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
     }
 };
